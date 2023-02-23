0a31844ed718f851b445e91949c76778bf0fbe14
Cristiano Goncalves <cristianogoncalves@pop-os.localdomain>
2023-02-22 21:43:56 -0300

    chore: setup deploy

---
+++ storybook-static/assets/Button.stories-abea02fc.js
@@ -0,0 +1,9 @@
+var o=Object.defineProperty;var n=(e,r)=>o(e,"name",{value:r,configurable:!0});import{s as a,j as s}from"./styled-components.browser.esm-4aaed480.js";import"./index-d7929fe6.js";import"./es.object.get-own-property-descriptor-1a76aaf5.js";const d=a.button`
+  border: 0;
+  background-color: ${({theme:e})=>e.colors.primary};
+  font-size: ${({theme:e})=>e.fontsSizes.md};
+  padding: 8px 16px;
+  border-radius: 10px;
+  color: #fff;
+`;function t({children:e,...r}){return s(d,{...r,children:e})}n(t,"Button");t.displayname="Button";try{t.displayName="Button",t.__docgenInfo={description:"",displayName:"Button",props:{theme:{defaultValue:null,description:"",name:"theme",required:!1,type:{name:"any"}},as:{defaultValue:null,description:"",name:"as",required:!1,type:{name:"string | ComponentType<any>"}},forwardedAs:{defaultValue:null,description:"",name:"forwardedAs",required:!1,type:{name:"string | ComponentType<any>"}}}}}catch{}const c={title:"Form/Button",component:t,args:{children:"Enviar"}},m={},y={},f=["Primary","Secondary"];export{m as Primary,y as Secondary,f as __namedExportsOrder,c as default};
+//# sourceMappingURL=Button.stories-abea02fc.js.map
